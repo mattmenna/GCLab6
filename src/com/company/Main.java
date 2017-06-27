f5756352ead40d37253c34406de1742a1f724ca7
Matthew Menna <mattmenna@gmail.com>
2017-06-27 15:00:27 -0400

    Working on adding to string.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ package com.company;
 Prompt the user for a word
 Translate the text to Pig Latin and display it on the console.
 Ask the user if he or she wants to translate another word.
+
+todo Does this need stringbuilder?
+
  */
 
 import java.util.Scanner;
@@ -23,13 +26,15 @@ public class Main {
 
             if (lowerCaseinput.substring(0, 1).matches("[aeiou]")) {
                 System.out.println("Vowel in first letter");
+                System.out.println(lowerCaseinput + "way");
             } else {
                 System.out.println("Consonant");
             }
-            scnr.nextLine();
+
+            scnr.nextLine(); // clears scanner so loop operates properly
             System.out.println("Do you want to continue? Please use y or n");
             cont = scnr.next().charAt(0);
-            scnr.nextLine();
+            scnr.nextLine(); // clears scanner so loop operates properly
 
         } while (cont != 'n');
 
