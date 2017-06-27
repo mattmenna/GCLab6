b4a82f086d9c8de17c63b1c94da1354b95a344c4
Matthew Menna <mattmenna@gmail.com>
2017-06-27 16:12:16 -0400

    Comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ Prompt the user for a word
 Translate the text to Pig Latin and display it on the console.
 Ask the user if he or she wants to translate another word.
 
-todo I think this is a deadend and need to redevolop with methods that work on individual words. Currently only depeneds on first char.
+todo I think this is a deadend and need to redevelop with methods that work on individual words. Currently only depeneds on first char.
 
 Try split method first and then work on individual words
 
